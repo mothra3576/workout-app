82a6745998fc4088248dc6028da8e5cdedb01152
mothra3576 <chihiroohara3576@gmail.com>
2026-09-01 16:59:54 +0900

    Fix config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Google Apps Scriptを公開した後、発行されたURLとCode.gsと同じトークンを入力します。
 window.WORKOUT_API_CONFIG = {
   url: "https://script.google.com/macros/s/AKfycbzsqgk1nP4monhnvQrYXoihELsCpGMQq1Pl6WaRIVYAyxz3lZ_ysJmNlXnHKKi2mmJv/exec",
-  token: "API_for_Muscle_Training_Mothra09091912000",
+  token: "ここに現在Code.gsで使用しているAPI_TOKEN",
 };
